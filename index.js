78d3942ff1d7564fbddf7da55033d203e4d13eb4
Akeem Lawrence <akeemwlawrence@gmail.com>
2022-11-01 08:38:03 -0400

    added GET for track's audio features

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 GET https://api.spotify.com/v1/playlists/{playlist_id}/tracks
 GET https://api.spotify.com/v1/tracks/{id}
+curl --request GET \
+  --url https://api.spotify.com/v1/audio-features \
+  --header 'Authorization: ' \
+  --header 'Content-Type: application/json'
